d61502047e13070731f63d58a161ed966ee00124
Bryan Ricker <bricker88@gmail.com>
2014-08-21 00:38:39 -0700

    Fix rake path

--- lib/dbsync.rb
+++ lib/dbsync.rb
@@ -14,7 +14,7 @@ module Dbsync
   if defined?(Rails)
     class Railtie < Rails::Railtie
       rake_tasks do
-        load File.expand_path("dbsync/rake_tasks.rb", __FILE__)
+        load File.expand_path("../dbsync/rake_tasks.rb", __FILE__)
       end
     end
   end
--- lib/dbsync/version.rb
+++ lib/dbsync/version.rb
@@ -1,3 +1,3 @@
 module Dbsync
-  VERSION = "1.0.0.beta3"
+  VERSION = "1.0.0.beta4"
 end
